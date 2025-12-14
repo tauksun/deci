69d24cab083a3b4c889391558118d722092f1b91
tauksun <tauksun@gmail.com>
2025-12-14 13:47:54 +0530

    async registeration

--- src/lcp/registration.hpp
+++ src/lcp/registration.hpp
@@ -1,10 +1,14 @@
 #ifndef LCP_REGISTRATION
 #define LCP_REGISTRATION
 
+#include "../common/encoder.hpp"
 #include <string>
 
 int lcpRegistration();
 
 int connectionRegistration(int connSockFd, std::string type, std::string lcpId);
+
+vector<QueryArrayElement> connRegisterationMessage(std::string type,
+                                                   std::string lcpId);
 
 #endif
